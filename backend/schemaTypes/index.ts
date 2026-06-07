64be9b161e5b5f7e07e501dedfa7d954b065a4ee
Cammy Docherty <cammy.docherty@klaviyo.com>
2026-06-07 15:49:17 +0100

    Remove user type

--- backend/schemaTypes/index.ts
+++ backend/schemaTypes/index.ts
@@ -2,6 +2,5 @@ import {currentlyPlayingType} from './currentlyPlaying'
 import {djType} from './dj'
 import {portfolioType} from './portfolio'
 import {setType} from './set'
-import {userType} from './user'
 
-export const schemaTypes = [currentlyPlayingType, djType, portfolioType, setType, userType]
+export const schemaTypes = [currentlyPlayingType, djType, portfolioType, setType]
